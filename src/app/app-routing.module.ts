e544e34f6ce9de676dbbc5099658a67f6c28a5b6
manish4648 <manish.aermani4648@gmail.com>
2025-03-08 22:07:34 +0530

    remove login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'login', component:LoginComponent},
   {path:'home',component:HomeComponent},
   {path:'search/:serachItem',component:HomeComponent},
